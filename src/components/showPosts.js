e661a058e6ace1de7d6cf70a04cbf7dbdd3b4689
Affan Sajid <affansajid@gmail.com>
2017-09-15 04:54:56 -0400

    Start first component

---
+++ src/components/showPosts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class showPosts extends Component {
+  render()
+}
+
+function mapStateToProps() {
+
+}
+
+export default connect(mapStateToProps)(showPosts);
