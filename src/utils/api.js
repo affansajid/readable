5daf73339bf648ffd65a3490852a734fb3eafd8e
Affan Sajid <affansajid@gmail.com>
2017-09-15 20:59:56 -0400

    Add API

---
+++ src/utils/api.js
@@ -0,0 +1,31 @@
+const API_ID = "my_readable_api_id"
+
+export function fetchPosts (category = 'none') {
+
+  if (category != 'none') {
+    return fetch(
+        `http://localhost:5001/${category}/posts`,
+        { headers: { 'Authorization': API_ID }}
+      )
+      .then((res) => res.json())
+      .then(({ hits }) => hits.map(({ recipe }) => recipe))
+  }
+  else {
+    return fetch(
+        'http://localhost:5001/posts',
+        { headers: { 'Authorization': API_ID }}
+      )
+      .then((res) => res.json())
+      .then(({ hits }) => hits.map(({ recipe }) => recipe))
+  }
+}
+
+export function fetchCategories () {
+
+  return fetch(
+      'http://localhost:5001/categories',
+      { headers: { 'Authorization': API_ID }}
+    )
+    .then((res) => res.json())
+    .then(({ hits }) => hits.map(({ recipe }) => recipe))
+}
