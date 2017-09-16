19e4f79b1c9999daae31220c777d05b1f5288ea9
Affan Sajid <affansajid@gmail.com>
2017-09-15 20:31:40 -0400

    Added static posts actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,22 @@ export const EDIT_COMMENT = "EDIT_COMMENT";
 export const DELETE_POST = "DELETE_POST";
 export const ADD_POST = "ADD_POST";
 export const EDIT_POST = "EDIT_POST";
+export const FETCH_POSTS = "FETCH_POSTS";
+
+
+
+export function fetchPosts (category) {
+	return {
+		type: FETCH_POSTS,  
+		payload: {
+      category: category,
+      posts: [
+        {
+          title: "Hello World!",
+          body: "This is a hello world post.",
+          author: "Affan"
+        }
+      ]
+    }
+	}
+}
